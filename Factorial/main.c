3da4536301a3d40d8e2ba90c6d49f72c79a2eb95
İsmail Hakkı DERELİ <ismailhdereli@gmail.com>
2019-04-06 19:07:55 +0300

    Update main.c

--- Factorial/main.c
+++ Factorial/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int factorial(int n);
+int Factorial(int n);
 
 int main(void){
-	printf("%d! = %d\n", 3, factorial(3));
+	printf("%d! = %d\n", 3, Factorial(3));
 	return 0;
 }
 
-int factorial(int n){
+int Factorial(int n){
 	if(n == 0)
 		return 1;
-	return n * factorial(n - 1);
+	return n * Factorial(n - 1);
 }
